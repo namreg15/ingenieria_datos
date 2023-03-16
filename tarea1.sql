83f83f0e7750c08e70f06535d3ce38923c7d5ff3
namreg15 <namreg.dor@gmail.com>
2023-03-15 23:19:16 -0500

    Update tarea1.sql

--- tarea1.sql
+++ tarea1.sql
@@ -3,7 +3,9 @@ CREATE TABLE tarea_01 (
   currency character varying, 
   rate character varying
 );
-
+INSERT INTO tarea_01 (currency, rate) VALUES('pruebas', 'ejemplo');
+INSERT INTO tarea_01 (currency, rate) VALUES('pruebas2', 'ejemplo2');
+INSERT INTO tarea_01 (currency, rate) VALUES('pruebas3', 'ejemplo2');
 -- Created from python:
 --	with open('exchange_rates.csv', "r") as f:
 --	rates = list([line.rstrip().split(';') for line in f])[1:]
@@ -11,6 +13,4 @@ CREATE TABLE tarea_01 (
 --	cur = rate[0]
 --	rat	= rate[1]
 --  print("INSERT INTO tarea_01 (currency, rate) VALUES('{}', '{}');".format(cur,rat))
-INSERT INTO tarea_01 (currency, rate) VALUES('pruebas', 'ejemplo');
-INSERT INTO tarea_01 (currency, rate) VALUES('pruebas2', 'ejemplo2');
-INSERT INTO tarea_01 (currency, rate) VALUES('pruebas3', 'ejemplo2');
+
