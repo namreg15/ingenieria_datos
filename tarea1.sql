62f2dfff85f5cfe7d5aef8b1a141faf39fd18a12
namreg15 <namreg.dor@gmail.com>
2023-03-15 22:25:53 -0500

    Add files via upload

---
+++ tarea1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tarea_1 (
+  currency character varying, 
+  rate character varying
+);
+
+-- Created from python:
+--	with open('exchange_rates.csv', "r") as f:
+--	rates = list([line.rstrip().split(';') for line in f])[1:]
+--	for rate in rates:
+--	cur = rate[0]
+--	rat	= rate[1]
+--  print("INSERT INTO tarea_1 (currency, rate) VALUES(cur, rat);
+
